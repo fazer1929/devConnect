26efdd558b9d5f9ec7c04823e123312e02cc275e
fazer1929 <abhishekagrawal8888@gmail.com>
2021-04-20 02:30:11 +0530

    add profile api

---
+++ backend/models/Profile.js
@@ -0,0 +1,106 @@
+const mongoose = require("mongoose");
+const ProfileSchema = new mongoose.Schema({
+	user: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "user",
+	},
+	education: [
+		{
+			school: {
+				type: String,
+				required: true,
+			},
+			degree: {
+				type: String,
+				required: true,
+			},
+			fieldofstudy: {
+				type: String,
+				required: true,
+			},
+			from: {
+				type: Date,
+				required: true,
+			},
+			to: {
+				type: Date,
+			},
+			current: {
+				type: Boolean,
+				default: false,
+			},
+			description: {
+				type: String,
+			},
+		},
+	],
+	location: {
+		type: String,
+	},
+	status: {
+		type: String,
+		required: true,
+	},
+	skills: {
+		type: [String],
+		required: true,
+	},
+	bio: {
+		type: String,
+	},
+	githubusername: {
+		type: String,
+	},
+	projects: [
+		{
+			title: {
+				type: String,
+				required: true,
+			},
+			tools: {
+				type: [String],
+			},
+			from: {
+				type: Date,
+			},
+			to: {
+				type: Date,
+			},
+			description: {
+				type: String,
+			},
+			links: {
+				type: [String],
+			},
+		},
+	],
+	socials: {
+		youtube: {
+			type: String,
+		},
+		twitter: {
+			type: String,
+		},
+		instagram: {
+			type: String,
+		},
+		stackoverflow: {
+			type: String,
+		},
+		facebook: {
+			type: String,
+		},
+		linkedin: {
+			type: String,
+		},
+		personalwebsite: {
+			type: String,
+		},
+	},
+	date: {
+		type: Date,
+		default: Date.now,
+	},
+});
+
+module.exports = Profile = mongoose.model("profile", ProfileSchema);
--- backend/routes/api/profile.js
+++ backend/routes/api/profile.js
@@ -1,10 +1,96 @@
 const router = require("express").Router();
+const auth = require("../../middleware/auth");
+const Profile = require("../../models/Profile");
+const User = require("../../models/Users");
+const { check, validationResult } = require("express-validator");
 
-//@route    GET api/auth
-// @desc    Test route
-// @access  Public
-router.get("/", (req, res) => {
-	res.send("Auth Route");
+//@route    GET api/profile/me
+// @desc    Get Current User profile
+// @access  Private
+router.get("/me", auth, async (req, res) => {
+	try {
+		const profile = await Profile.findOne({
+			user: req.user.id,
+		}).populate("user", ["name", "avatar"]);
+		if (!profile) {
+			return res.status(400).json({ msg: "There is no profile for this user" });
+		}
+		return res.json(profile);
+	} catch (error) {
+		console.error(error.message);
+		res.status(500).send("Server Error");
+	}
 });
+
+//@route    GET api/profile/me
+// @desc    Create/Update User profile
+// @access  Private
+router.post(
+	"/",
+	[
+		auth,
+		[
+			check("status", "Status is Required").notEmpty(),
+			check("skills", "Skills is Required").notEmpty(),
+		],
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		const {
+			githubusername,
+			skills,
+			status,
+			location,
+			instagram,
+			linkedin,
+			facebook,
+			twitter,
+			stackoverflow,
+			bio,
+			youtube,
+			personalwebsite,
+		} = req.body;
+		const profileFields = {};
+		profileFields.user = req.user.id;
+		if (skills) profileFields.skills = skills;
+		if (bio) profileFields.bio = bio;
+		if (status) profileFields.status = status;
+		if (githubusername) profileFields.githubusername = githubusername;
+		if (location) profileFields.location = location;
+		profileFields.socials = {};
+		if (youtube) profileFields.socials.youtube = youtube;
+		if (stackoverflow) profileFields.socials.stackoverflow = stackoverflow;
+		if (facebook) profileFields.socials.facebook = facebook;
+		if (instagram) profileFields.socials.instagram = instagram;
+		if (twitter) profileFields.socials.twitter = twitter;
+		if (personalwebsite)
+			profileFields.socials.personalwebsite = personalwebsite;
+		if (linkedin) profileFields.socials.linkedin = linkedin;
+		if (skills)
+			profileFields.skills = skills.split(",").map((skill) => skill.trim());
+
+		try {
+			let profile = await Profile.findOne({ user: req.user.id });
+			if (profile) {
+				profile = await Profile.findOneAndUpdate(
+					{ user: req.user.id },
+					{ $set: profileFields },
+					{ new: true },
+				);
+				return res.json(profile);
+			}
+			profile = new Profile(profileFields);
+			await profile.save();
+			return res.json(profile);
+		} catch (error) {
+			console.log(error.message);
+			return res.status(500).send("Server Error");
+		}
+	},
+);
 
 module.exports = router;
